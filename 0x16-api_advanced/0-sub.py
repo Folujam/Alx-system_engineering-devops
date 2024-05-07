b908bb27c85e9fe59c8377b3043383556c2773f2
folujam <olusway@gmail.com>
2024-05-07 15:25:32 +0100

    init && 0

---
+++ 0x16-api_advanced/0-sub.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""queries the Reddit API"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers={'User-Agent': 'my-app'})
+    try:
+        return response.json()['data']['subscribers']
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+this is the advanced api(0x16-api_advanced) directory
