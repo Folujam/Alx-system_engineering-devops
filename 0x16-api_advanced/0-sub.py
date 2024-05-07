290f6fe6f6530a810f3075edf6cc6a4e34fb36ca
folujam <olusway@gmail.com>
2024-05-07 22:29:28 +0100

    try removed

--- 0x16-api_advanced/0-sub.py
+++ 0x16-api_advanced/0-sub.py
@@ -5,9 +5,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    """
+    returns the number of subscribers
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers={'User-Agent': 'my-app'})
+    
     if response.status_code == 200:
-        return (response.json().get('data').get('subscribers'))
-    return (0)
+            return (response.json().get("data").get("subscribers"))
