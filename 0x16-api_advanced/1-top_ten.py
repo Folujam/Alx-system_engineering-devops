511ff2ae6e8ca7ac47e95e18a766fbc3d9f4dfd8
folujam <olusway@gmail.com>
2024-05-07 23:02:30 +0100

    1 go

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""queries the Reddit API"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints top ten post titles listed
+    """
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    head = {'User-Agent': 'my-app'}
+    parameter = {'limit': 10}
+    response = requests.get(url, headers=head, params=parameter)
+    posts = response.json().get('data', {}).get('children', None)
+    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
+        print(None)
+    else:
+        for post in posts:
+            print(post.get('data', {}).get('title', None))
