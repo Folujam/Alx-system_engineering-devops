14162c02c487232febff2777dd30364eaca5ddbf
folujam <olusway@gmail.com>
2024-05-07 15:29:57 +0100

    reduced long line

--- 0x16-api_advanced/0-sub.py
+++ 0x16-api_advanced/0-sub.py
@@ -6,7 +6,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
-    response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers={'User-Agent': 'my-app'})
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers={'User-Agent': 'my-app'})
     try:
         return response.json()['data']['subscribers']
     except Exception:
